e2512d70c5cc6d414d435ad51b5d080451175014
IkkyLuiz <ikkylh26@gmail.com>
2023-10-11 17:01:42 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+class Calculadora
+{
+    static void Main(string[] args)
+    {
+        // Declaração das variáveis
+        int x, y;
+        string operador;
+
+        // Solicita os valores
+        Console.WriteLine("Insira o primeiro número:");
+        x = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Insira o segundo número:");
+        y = Convert.ToInt32(Console.ReadLine());
+
+        // Solicita o operador
+        Console.WriteLine("Insira o operador (+, -, *, /):");
+        operador = Console.ReadLine();
+
+        // Realiza o cálculo
+        switch (operador)
+        {
+            case "+":
+                Console.WriteLine("O resultado é: " + (x + y));
+                break;
+            case "-":
+                Console.WriteLine("O resultado é: " + (x - y));
+                break;
+            case "*":
+                Console.WriteLine("O resultado é: " + (x * y));
+                break;
+            case "/":
+                Console.WriteLine("O resultado é: " + (x / y));
+                break;
+            default:
+                Console.WriteLine("Operador inválido");
+                break;
+        }
+    }
+}
+
